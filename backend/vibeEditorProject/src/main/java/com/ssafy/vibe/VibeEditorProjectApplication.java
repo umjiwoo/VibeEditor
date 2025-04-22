349a1cafc09914aacd8769a16eb3c942f4bec10b
uts417923 <uts417923@gmail.com>
2025-04-22 13:33:11 +0900

    init

---
+++ backend/vibeEditorProject/src/main/java/com/ssafy/vibe/VibeEditorProjectApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.vibe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VibeEditorProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VibeEditorProjectApplication.class, args);
+    }
+
+}
---
+++ backend/vibeEditorProject/src/test/java/com/ssafy/vibe/VibeEditorProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.vibe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VibeEditorProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
