d6635ac2df89f8f9ee8c36d4b136e84d24699991
윤윤호 <yunho_yun@naver.com>
2025-04-23 15:08:43 +0900

    feat: AOP 설정

--- backend/src/main/java/com/ssafy/vibe/VibeEditorProjectApplication.java
+++ backend/src/main/java/com/ssafy/vibe/VibeEditorProjectApplication.java
@@ -2,12 +2,16 @@ package com.ssafy.vibe;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableAspectJAutoProxy
+@EnableJpaAuditing
 public class VibeEditorProjectApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(VibeEditorProjectApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(VibeEditorProjectApplication.class, args);
+	}
 
 }
