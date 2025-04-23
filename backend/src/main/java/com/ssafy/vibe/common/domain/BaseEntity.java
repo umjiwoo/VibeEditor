da65c857a8bad4506aa37e78655414152417b391
윤윤호 <yunho_yun@naver.com>
2025-04-23 13:23:32 +0900

    S12P31A503-57: feat: base entity 정의

---
+++ backend/src/main/java/com/ssafy/vibe/common/domain/BaseEntity.java
@@ -0,0 +1,31 @@
+package com.ssafy.vibe.common.domain;
+
+import java.time.Instant;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@EntityListeners(AuditingEntityListener.class)
+@MappedSuperclass
+public abstract class BaseEntity {
+
+	@CreatedDate
+	@Column(name = "created_at", updatable = false, nullable = false)
+	private Instant createdAt;
+
+	@LastModifiedDate
+	@Column(name = "updated_at", nullable = false)
+	private Instant updatedAt;
+
+	@Column(name = "is_active")
+	private Boolean isActive;
+}
