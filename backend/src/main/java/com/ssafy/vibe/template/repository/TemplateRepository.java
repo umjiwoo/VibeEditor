f410c5d9d5b258660b937228afab4ec0156d9b51
윤윤호 <yunho_yun@naver.com>
2025-04-25 11:21:39 +0900

    feat: TemplateRepository 정의

---
+++ backend/src/main/java/com/ssafy/vibe/template/repository/TemplateRepository.java
@@ -0,0 +1,20 @@
+package com.ssafy.vibe.template.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import com.ssafy.vibe.template.domain.TemplateEntity;
+
+@Repository
+public interface TemplateRepository extends JpaRepository<TemplateEntity, Long> {
+	@Query("select te from TemplateEntity te where te.user.id = :userId and te.isActive = true")
+	List<TemplateEntity> findByUserIdAndActive(@Param("userId") Long userId);
+
+	@Query("select te from TemplateEntity te where te.user.id = :userId and te.id = :templateId and te.isActive = true")
+	Optional<TemplateEntity> findByIdAndActive(@Param("userId") Long userId, @Param("templateId") Long templateId);
+}
