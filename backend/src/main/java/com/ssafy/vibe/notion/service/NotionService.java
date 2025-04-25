7566849720e3a8b7d121b9c1aac4181c1a0a2a5b
leedonghyun <skydh507@gmail.com>
2025-04-25 16:21:50 +0900

    feat: 노션 연동 service 코드 생성 / api 키 저장 로직 생성

---
+++ backend/src/main/java/com/ssafy/vibe/notion/service/NotionService.java
@@ -0,0 +1,9 @@
+package com.ssafy.vibe.notion.service;
+
+import com.ssafy.vibe.notion.service.dto.NotionConnectInfoDto;
+
+public interface NotionService {
+
+	void saveNotionKey(NotionConnectInfoDto dto);
+
+}
---
+++ backend/src/main/java/com/ssafy/vibe/notion/service/NotionServiceImpl.java
@@ -0,0 +1,30 @@
+package com.ssafy.vibe.notion.service;
+
+import org.springframework.stereotype.Service;
+
+import com.ssafy.vibe.common.exception.BadRequestException;
+import com.ssafy.vibe.common.exception.ExceptionCode;
+import com.ssafy.vibe.common.util.AesEncryptor;
+import com.ssafy.vibe.notion.service.dto.NotionConnectInfoDto;
+import com.ssafy.vibe.user.domain.UserEntity;
+import com.ssafy.vibe.user.repository.UserRepository;
+
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class NotionServiceImpl implements NotionService {
+	private final AesEncryptor encryptor;
+	private final UserRepository userRepository;
+
+	@Override
+	@Transactional
+	public void saveNotionKey(NotionConnectInfoDto info) {
+		UserEntity user = userRepository.findById(info.getUserId())
+			.orElseThrow(() -> new BadRequestException(ExceptionCode.USER_NOT_FOUND));
+		String encryptedKey = encryptor.encrypt(info.getNotionApiKey());
+		user.setApiKey(encryptedKey);
+		userRepository.save(user);
+	}
+}
