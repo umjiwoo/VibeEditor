b571cbf87ee64152edbe687abb2f6bbced703abd
윤윤호 <yunho_yun@naver.com>
2025-04-25 16:37:41 +0900

    feat: 템플릿 생성 및 수정 request에 검증 추가

--- backend/src/main/java/com/ssafy/vibe/template/controller/request/CreateTemplateRequest.java
+++ backend/src/main/java/com/ssafy/vibe/template/controller/request/CreateTemplateRequest.java
@@ -3,10 +3,7 @@ package com.ssafy.vibe.template.controller.request;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
-public record UpdateTemplateRequest(
-	@NotBlank(message = "{required}")
-	Long templateId,
-
+public record CreateTemplateRequest(
 	@NotBlank(message = "{required}")
 	@Size(max = 30, message = "{max.length}")
 	String templateName
