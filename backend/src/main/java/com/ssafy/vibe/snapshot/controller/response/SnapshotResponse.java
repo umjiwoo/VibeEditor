22e6db4f71673c829fd4c3c1614c40367c146c7a
윤윤호 <yunho_yun@naver.com>
2025-04-28 10:43:05 +0900

    feat: Snapshot 상세 조회 response DTO 정의

---
+++ backend/src/main/java/com/ssafy/vibe/snapshot/controller/response/SnapshotResponse.java
@@ -0,0 +1,30 @@
+package com.ssafy.vibe.snapshot.controller.response;
+
+import java.time.Instant;
+
+import com.ssafy.vibe.snapshot.domain.SnapshotEntity;
+import com.ssafy.vibe.snapshot.domain.SnapshotType;
+
+import lombok.Builder;
+
+@Builder
+public record SnapshotResponse(
+	Long snapshotId,
+	String snapshotName,
+	SnapshotType snapshotType,
+	String content,
+	Instant createdAt,
+	Instant updatedAt
+) {
+	public static SnapshotResponse from(SnapshotEntity snapshot) {
+		return SnapshotResponse
+			.builder()
+			.snapshotId(snapshot.getId())
+			.snapshotName(snapshot.getSnapshotName())
+			.snapshotType(snapshot.getSnapshotType())
+			.content(snapshot.getContent())
+			.createdAt(snapshot.getCreatedAt())
+			.updatedAt(snapshot.getUpdatedAt())
+			.build();
+	}
+}
