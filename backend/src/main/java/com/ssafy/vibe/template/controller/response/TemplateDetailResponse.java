5215d5f5da560138c2416003251db0a49303758e
윤윤호 <yunho_yun@naver.com>
2025-04-28 12:40:30 +0900

    fix: template 조회 시 active 상태인 스냅샷과 프롬프트만 조회하도록 변경

--- backend/src/main/java/com/ssafy/vibe/template/controller/response/TemplateDetailResponse.java
+++ backend/src/main/java/com/ssafy/vibe/template/controller/response/TemplateDetailResponse.java
@@ -2,7 +2,9 @@ package com.ssafy.vibe.template.controller.response;
 
 import java.util.List;
 
+import com.ssafy.vibe.prompt.domain.PromptEntity;
 import com.ssafy.vibe.prompt.service.dto.PromptDTO;
+import com.ssafy.vibe.snapshot.domain.SnapshotEntity;
 import com.ssafy.vibe.snapshot.service.dto.SnapshotDTO;
 import com.ssafy.vibe.template.domain.TemplateEntity;
 
@@ -18,8 +20,13 @@ public record TemplateDetailResponse(
 		return TemplateDetailResponse
 			.builder()
 			.templateName(template.getTemplateName())
-			.snapshotList(template.getSnapshots().stream().map(SnapshotDTO::from).toList())
-			.promptList(template.getPrompts().stream().map(PromptDTO::from).toList())
+			.snapshotList(
+				template.getSnapshots().stream()
+					.filter(SnapshotEntity::getIsActive)
+					.map(SnapshotDTO::from).toList())
+			.promptList(template.getPrompts().stream()
+				.filter(PromptEntity::getIsActive)
+				.map(PromptDTO::from).toList())
 			.build();
 	}
 }
