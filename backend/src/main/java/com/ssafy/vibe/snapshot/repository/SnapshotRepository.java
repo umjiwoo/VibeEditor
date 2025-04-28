a6710e1140b0d015b06084167dfeee03917add4b
윤윤호 <yunho_yun@naver.com>
2025-04-28 15:20:41 +0900

    feat: 사용자의 스냅샷 조회 기능 구현

--- backend/src/main/java/com/ssafy/vibe/snapshot/repository/SnapshotRepository.java
+++ backend/src/main/java/com/ssafy/vibe/snapshot/repository/SnapshotRepository.java
@@ -1,5 +1,6 @@
 package com.ssafy.vibe.snapshot.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -11,6 +12,9 @@ import com.ssafy.vibe.snapshot.domain.SnapshotEntity;
 
 @Repository
 public interface SnapshotRepository extends JpaRepository<SnapshotEntity, Long> {
+
+	@Query("select se from SnapshotEntity se where se.user.id = :userId and se.isActive = true")
+	List<SnapshotEntity> findByUserIdAndActive(@Param("userId") Long userId);
 
 	@Query("select se from SnapshotEntity se where se.user.id = :userId and se.id = :snapshotId and se.isActive = true")
 	Optional<SnapshotEntity> findByIdAndActive(@Param("userId") Long userId, @Param("snapshotId") Long snapshotId);
