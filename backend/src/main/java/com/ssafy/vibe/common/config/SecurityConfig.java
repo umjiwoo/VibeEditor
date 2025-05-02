bd057696884b594b652eb16d697d974efeed365a
윤윤호 <yunho_yun@naver.com>
2025-05-02 16:15:18 +0900

    S12P31A503-142: feat: Actuator(Health check) 관련 URL JWT Filter 예외 처리

--- backend/src/main/java/com/ssafy/vibe/common/config/SecurityConfig.java
+++ backend/src/main/java/com/ssafy/vibe/common/config/SecurityConfig.java
@@ -53,7 +53,7 @@ public class SecurityConfig {
 					.failureHandler(oAuth2AuthenticationFailureHandler))
 			.authorizeHttpRequests(auth ->
 				auth
-					.requestMatchers("/oauth2/**", "/login/oauth2/**")
+					.requestMatchers("/oauth2/**", "/login/oauth2/**", "/api/health", "/api/prometheus")
 					.permitAll()
 					.anyRequest()
 					.authenticated())
