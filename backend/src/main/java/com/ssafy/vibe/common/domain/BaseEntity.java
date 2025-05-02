5bce7998129bcba659ebd2b40d039cd547fc7de0
윤윤호 <yunho_yun@naver.com>
2025-05-02 10:51:24 +0900

    feat: BaseEntity의 활성여부 표현을 isDeleted로 수정

--- backend/src/main/java/com/ssafy/vibe/common/domain/BaseEntity.java
+++ backend/src/main/java/com/ssafy/vibe/common/domain/BaseEntity.java
@@ -26,6 +26,6 @@ public abstract class BaseEntity {
 	@Column(name = "updated_at", nullable = false)
 	private Instant updatedAt;
 
-	@Column(name = "is_active")
-	private Boolean isActive;
+	@Column(name = "is_deleted")
+	private Boolean isDeleted = false;
 }
