7519a89196cb14fdf1a2bc4b7b71516c51e598eb
윤윤호 <yunho_yun@naver.com>
2025-05-07 10:46:48 +0900

    feat: updatedAt 기준 내림차순 정렬

--- backend/src/main/java/com/ssafy/vibe/template/controller/response/TemplateDetailResponse.java
+++ backend/src/main/java/com/ssafy/vibe/template/controller/response/TemplateDetailResponse.java
@@ -1,9 +1,12 @@
 package com.ssafy.vibe.template.controller.response;
 
 import java.time.Instant;
+import java.util.Comparator;
 import java.util.List;
 
+import com.ssafy.vibe.prompt.domain.PromptEntity;
 import com.ssafy.vibe.prompt.service.dto.PromptDTO;
+import com.ssafy.vibe.snapshot.domain.SnapshotEntity;
 import com.ssafy.vibe.snapshot.service.dto.SnapshotDTO;
 import com.ssafy.vibe.template.domain.TemplateEntity;
 
@@ -24,9 +27,11 @@ public record TemplateDetailResponse(
 			.snapshotList(
 				template.getSnapshots().stream()
 					.filter(snapshot -> !snapshot.getIsDeleted())
+					.sorted(Comparator.comparing(SnapshotEntity::getUpdatedAt).reversed())
 					.map(SnapshotDTO::from).toList())
 			.promptList(template.getPrompts().stream()
 				.filter(prompt -> !prompt.getIsDeleted())
+				.sorted(Comparator.comparing(PromptEntity::getUpdatedAt).reversed())
 				.map(PromptDTO::from).toList())
 			.createdAt(template.getCreatedAt())
 			.updatedAt(template.getUpdatedAt())
