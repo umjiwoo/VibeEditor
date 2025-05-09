5c5527cfec69358fc43113e87644da104f6348ee
uts417923 <uts417923@gmail.com>
2025-05-09 14:14:09 +0900

    feat: AI 이용 포스트 생성 후 ai-post 테이블에 저장하기 & 입력받은 prompt_attach, prompt_option 없을 시 기본 값 세팅

--- backend/src/main/java/com/ssafy/vibe/prompt/controller/response/CreatedPostResponse.java
+++ backend/src/main/java/com/ssafy/vibe/prompt/controller/response/CreatedPostResponse.java
@@ -8,11 +8,13 @@ import lombok.Getter;
 @Builder
 @AllArgsConstructor
 public class CreatedPostResponse {
+	private Long postId;
 	private String postTitle;
 	private String postContent;
 
-	public static CreatedPostResponse from(String postTitle, String postContent) {
+	public static CreatedPostResponse from(Long postId, String postTitle, String postContent) {
 		return CreatedPostResponse.builder()
+			.postId(postId)
 			.postTitle(postTitle)
 			.postContent(postContent)
 			.build();
