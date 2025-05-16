5122d06b33c577b65b60f4cacd6ae948b4ef1476
윤윤호 <yunho_yun@naver.com>
2025-05-16 10:10:05 +0900

    feat: 기본 제공 AI 등록 기능 구현
    - Anthropic으로 하드코딩되어 있음

--- backend/src/main/java/com/ssafy/vibe/user/service/UserAiProviderService.java
+++ backend/src/main/java/com/ssafy/vibe/user/service/UserAiProviderService.java
@@ -7,6 +7,8 @@ import com.ssafy.vibe.user.controller.request.UserAiUpdateRequest;
 import com.ssafy.vibe.user.controller.response.UserAiResponse;
 
 public interface UserAiProviderService {
+	void registerDefaultAPIKey(Long userId);
+
 	void registerUserAPIKey(Long userId, UserAiCreateRequest request);
 
 	void updateUserAPIKey(Long userId, UserAiUpdateRequest request);
